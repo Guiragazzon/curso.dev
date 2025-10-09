edd93e8b3e07e254140f3fdb8742ab539b9b312d
Guiragazzon <guilhermehragazzon@gmail.com>
2025-10-09 02:29:05 +0000

    modifiquei a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma risadinha</h1>
+    return <h1>Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma risadinha!!</h1>
 }
 export default Home;
