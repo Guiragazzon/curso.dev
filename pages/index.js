652fe5df5807696b811056b405802b5af015917e
Guiragazzon <guilhermehragazzon@gmail.com>
2025-10-09 02:33:32 +0000

    ajustei novamente com amend

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma risadinha!!</h1>
+    return <h1>Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma risadinha, aqui vai mais um teste em! agora é só pra fazer amend</h1>
 }
 export default Home;
