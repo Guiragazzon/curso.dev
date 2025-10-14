9dd04faf557eaf9016045077e9d4a16f67a764ef
Guiragazzon <guilhermehragazzon@gmail.com>
2025-10-14 00:58:01 +0000

    adiciona script `lint:check` e `lint:fiz`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
 function Home() {
-    return <h1>Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma risadinha, aqui vai mais um teste em! agora é só pra fazer amend</h1>
+  return (
+    <h1>
+      Olá minha esposa, eu amo você e você ai melhorar!! Se você me ama, da uma
+      risadinha, aqui vai mais um teste em! agora é só pra fazer amend
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("teste");
 }
 export default Home;
